7b138c9ddadb3f21e5c4aa5f7217e5005ae6787c
Soumallya Hazra <soumallyahazra2217@gmail.com>
2022-07-17 15:44:51 +0530

    Updates

---
+++ precache-manifest.4ef42a1982526a78e3a0804133a959b6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acbbcb2df2d917f766aedb1d5a04644c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce506385fe12ae1c4baa",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce506385fe12ae1c4baa",
+    "url": "/static/js/main.3b5880a6.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "3e47421df3f4ad7bf544d1359be61da0",
+    "url": "/static/media/resume.3e47421d.pdf"
+  },
+  {
+    "revision": "0b37d0053ff0d5698c5fbb027c8e22b1",
+    "url": "/static/media/soumallya.0b37d005.png"
+  }
+]);
